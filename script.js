a0ada4e2bc1467638ff82eb5f6672599ae31623a
Aline Furquim <aline1080@gmail.com>
2021-08-03 15:34:30 -0300

    Javascript aplication

---
+++ script.js
@@ -0,0 +1,68 @@
+let validatorFunction = {
+    handleSubmit:(event) => {
+        event.preventDefault();
+
+        let send = true;
+        let inputs = form.querySelectorAll('input');
+
+        validatorFunction.clearErrors();
+
+        for(let i=0; i < inputs.length; i++) {
+            let input = inputs[i];
+            let check = validatorFunction.checkInput(input);
+            if(check !== true) {
+                send = false;
+                validatorFunction.showError(input, check);
+            }
+        }        
+        //send = false;
+        if(send) {
+            form.submit();
+        }
+    },
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+        if (rules !== null) {
+            rules = rules.split('|');
+            for(let k in rules) {
+                let rulesDetails = rules[k].split('=');
+                switch(rulesDetails[0]) {
+                    case 'required':
+                        if(input.value == '') {
+                            return 'Campo obrigatório'
+                        }
+                    break
+                    case 'min':
+                        if(input.value.length < rulesDetails[1]) {
+                            return 'Campo deve ter pelo menos '+rulesDetails[1]+' caracteres';
+                        }
+                    break;
+                }
+            }
+        }
+        return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0; i < inputs.length; i++) {
+            inputs[i].styles = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
+    }
+};
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', validatorFunction.handleSubmit);
